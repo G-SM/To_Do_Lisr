9978f35bc20886ce85cee72fc31aa7acfe997e68
G-SM <mishra28sarthak@gmail.com>
2022-02-21 14:36:56 +0530

    Basic Structure Ready

---
+++ script.js
@@ -0,0 +1,17 @@
+let ulTasks = $('#ulTasks')
+let btnAdd = $('#btnAdd')
+let btnClear = $('#btnClear')
+let inpNewTask = $('#inpNewTask')
+
+btnAdd.click(()=>{
+    let listItem = $('<li>',{
+        'class':'list-group-item',
+        text: inpNewTask.val()
+    })
+    ulTasks.append(listItem)
+    inpNewTask.val("")
+})
+
+btn.btnClear.click(()=>{
+    inpNewTask.val("")
+})
