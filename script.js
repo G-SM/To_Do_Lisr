8847fd2d6081d08c319db49a0c87217ccafb3bb9
G-SM <mishra28sarthak@gmail.com>
2022-02-21 15:56:34 +0530

    Enter Functionality added

--- script.js
+++ script.js
@@ -2,16 +2,40 @@ let ulTasks = $('#ulTasks')
 let btnAdd = $('#btnAdd')
 let btnClear = $('#btnClear')
 let inpNewTask = $('#inpNewTask')
+btnAdd.click(adbtn)
+function adbtn() {
+    if(inpNewTask.val().trim()!==""){
+        let listItem = $('<li>',{
+            'class':'list-group-item',
+            text: inpNewTask.val()
+        })
+        listItem.click((event)=>{
+            
+            $(event.target).toggleClass("done")
+        })
+    
+        ulTasks.append(listItem)
+        inpNewTask.val("")
+    }   
+    else{
+        inpNewTask.val("")
+    }
 
-btnAdd.click(()=>{
-    let listItem = $('<li>',{
-        'class':'list-group-item',
-        text: inpNewTask.val()
-    })
-    ulTasks.append(listItem)
-    inpNewTask.val("")
+
+}
+
+
+
+
+inpNewTask.keypress((e)=>{
+    if(e.which==13){
+        adbtn()
+    }
 })
 
-btn.btnClear.click(()=>{
+
+
+
+btnClear.click(()=>{
     inpNewTask.val("")
 })
