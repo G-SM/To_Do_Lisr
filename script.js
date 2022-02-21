400758ec3f2403f8c0e47047de97fe2ba5efb4ef
G-SM <mishra28sarthak@gmail.com>
2022-02-21 18:01:57 +0530

    New Button Functionalities Added

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let ulTasks = $('#ulTasks')
 let btnAdd = $('#btnAdd')
-let btnClear = $('#btnClear')
+let btnReset = $('#btnReset')
 let inpNewTask = $('#inpNewTask')
+let btnClean = $('#btnClean')
+let btnSort = $("#btnSort")
+
 btnAdd.click(adbtn)
 function adbtn() {
+
     if(inpNewTask.val().trim()!==""){
+
         let listItem = $('<li>',{
             'class':'list-group-item',
             text: inpNewTask.val()
@@ -16,9 +21,12 @@ function adbtn() {
     
         ulTasks.append(listItem)
         inpNewTask.val("")
+        $('#btnReset').toggleClass("disabled",true)
+        $('#btnAdd').toggleClass("disabled",true)        
     }   
     else{
-        inpNewTask.val("")
+        
+
     }
 
 
@@ -33,9 +41,40 @@ inpNewTask.keypress((e)=>{
     }
 })
 
+function clearDone(){
+    $('#ulTasks .done').remove()
+}
 
 
-
-btnClear.click(()=>{
+btnReset.click(()=>{
     inpNewTask.val("")
+    $('#btnReset').toggleClass("disabled",true)
+    $('#btnAdd').toggleClass("disabled",true)
+})
+
+btnClean.click(clearDone)
+
+btnSort.click(()=>{
+    $('#ulTasks .done').appendTo(ulTasks)
+})
+
+function toggleResetBtn(enabled){
+    if(enabled){
+        btnReset.prop('disabled',false)
+    }
+    else{
+        btnReset.prop('disabled',true)
+    }
+}
+inpNewTask.on('input',()=>{
+    if(inpNewTask.val().trim()==""){
+        $('#btnReset').toggleClass("disabled",true)
+        $('#btnAdd').toggleClass("disabled",true)
+
+    }
+    else{
+        $('#btnReset').toggleClass("disabled",false)
+        $('#btnAdd').toggleClass("disabled",false)
+    }
+
 })
